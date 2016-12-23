7dfd8db3b43b5c2a6378cb1f368274a467ee3aa5
ezowolf <wiggler@live.nl>
2016-12-23 12:07:56 +0100

    Added terrain class

---
+++ scripts/objects/terrain.js
@@ -0,0 +1,17 @@
+terrainFunctions = {
+    terrainDestruct: function(x, y, size, terrainToDestroy) {
+        //this function destroys terrain in a circular form.
+        terrainToDestroy.blendDestinationOut();
+        terrainToDestroy.land.circle(x, y, size, 'rgba(0, 0, 0, 255');
+        terrainToDestroy.land.blendReset();
+        terrainToDestroy.land.update();
+    },
+
+    terrainCollide: function(x, y, terrainForCollision, otherObject) {
+        //stops moving when colliding with terrain.
+        var rgba = terrainForCollision.getPixel(x, y);
+        if (rgba.a == 255) {
+            otherObject.reset(x, y);
+        }
+    }
+}
--- scripts/scenes/game.js
+++ scripts/scenes/game.js
@@ -1,22 +1,29 @@
-var gameScene =
+var gameSceneVarHolder =
 {
-//"Awake" before start
-preload: function()
-{
-
-},
-
-//Start function
-create: function()
-{
-
-},
-
-//Called every frame
-update: function()
-{
-  console.log("Succes");
+  gameScene.land = null;
 }
+
+var gameScene = {
+    //"Awake" before start
+    preload: function() {
+        gameScene.game.renderer.renderSession.roundPixels = true;
+        //roundPixels is needed for destructible terrain
+        gameScene.physics.startSystem(Phaser.Physics.ARCADE);
+        gameScene.physics.arcade.gravity.y = 100;
+        //Start physics
+
+
+    },
+
+    //Start function
+    create: function() {
+      gameScene.land = game.add.bitmapData(0,0);
+    },
+
+    //Called every frame
+    update: function() {
+
+    }
 
 
 
